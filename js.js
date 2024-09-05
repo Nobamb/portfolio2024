7f24d73593d886f667a8e67a3e1244a9255ae8d8
JEONG <jsh12345644@gmail.com>
2024-09-05 10:58:29 +0900

    포트폴리오 사이트 추가 수정 및 파비콘 추가
    포트폴리오 사이트 디자인을 위한 일부 코드 수정 및 모든 사이트에 파비콘 추가

--- js.js
+++ js.js
@@ -1,56 +1,139 @@
 $(document).ready(function () {
 
 
-  
 
-  // 타이핑 글자
+
+
+
+
+
+
+
+
+  /** 이 함수는 글자를 타이핑시켜주는 이펙트를 구현시켜준다. 
+   첫번째 파라미터 : 구현시킬 글자, 
+   두번째 파라미터 : 옮길 글자,
+   세번째 파라미터 : 타이핑 속도
+  */
+
+
+  function txtTypeEffect(txt, typeToTxt, time) {
+
+    // 글자를 배열로 지정
+    const typingTxt = txt.split('');
+
+    // 글자 길이 선언
+    const typingLength = typingTxt.length;
+
+
+    // 새로 넣을 배열
+    let textThis = [];
+
+    // for문으로 글자 길이만큼
+    for (let i = 0; i < typingLength; i++) {
+
+
+
+      // 새로 넣을 빈 배열에 기존 글자의 배열을 하나하나씩 넣음
+      textThis[i] = typingTxt[i];
+
+
+      // 부를 글자 지정,
+      let typing = '';
+
+      // 부를 글자에 새로 넣은 배열의 값을 대입
+      textThis.map((ele) => typing += ele);
+
+
+
+
+      // 0.3초마다 글자가 하나씩 들어가게 되어 타이핑 효과를 냄
+      setTimeout(() => {
+
+        $(typeToTxt).text(typing);
+
+
+
+      }, time * i)
+
+    }
+
+
+
+  }
+
+
+
+
   const welcome = 'Welcome';
 
 
-
-  // 글자를 배열로 지정
-  const typingTxt = welcome.split('');
-
-
-  // 글자 길이 선언
-  const typingLength = typingTxt.length;
+  const introLen = welcome.length;
 
 
 
-  // 새로 넣을 배열
-  let textThis = [];
+  console.log(introLen);
+
+
+  txtTypeEffect(welcome, $('h1'), 300);
 
 
 
 
-  // for문으로 글자 길이만큼
-  for (let i = 0; i < typingLength; i++) {
 
 
 
-    // 새로 넣을 빈 배열에 기존 글자의 배열을 하나하나씩 넣음
-    textThis[i] = typingTxt[i];
 
 
-    // 부를 글자 지정,
-    let typing = '';
-
-    // 부를 글자에 새로 넣은 배열의 값을 대입
-    textThis.map((ele) => typing += ele);
-
-
-    
-
-    // 0.3초마다 글자가 하나씩 들어가게 되어 타이핑 효과를 냄
-    setTimeout(() => {
-
-      $('h1').text(typing);
+  // // 타이핑 글자
+  // const welcome = 'Welcome';
 
 
 
-    }, 300 * i)
+  // // 글자를 배열로 지정
+  // const typingTxt = welcome.split('');
 
-  }
+
+  // // 글자 길이 선언
+  // const typingLength = typingTxt.length;
+
+
+
+  // // 새로 넣을 배열
+  // let textThis = [];
+
+
+
+
+  // // for문으로 글자 길이만큼
+  // for (let i = 0; i < typingLength; i++) {
+
+
+
+  //   // 새로 넣을 빈 배열에 기존 글자의 배열을 하나하나씩 넣음
+  //   textThis[i] = typingTxt[i];
+
+
+  //   // 부를 글자 지정,
+  //   let typing = '';
+
+  //   // 부를 글자에 새로 넣은 배열의 값을 대입
+  //   textThis.map((ele) => typing += ele);
+
+
+
+
+  //   // 0.3초마다 글자가 하나씩 들어가게 되어 타이핑 효과를 냄
+  //   setTimeout(() => {
+
+  //     $('h1').text(typing);
+
+
+
+  //   }, 300 * i)
+
+  // }
+
 
 
 
@@ -64,7 +147,7 @@ $(document).ready(function () {
 
     $('.cursor').css({ 'right': '-60px' });
 
-  }, 300 * (typingLength));
+  }, 300 * introLen);
 
 
 
@@ -86,7 +169,13 @@ $(document).ready(function () {
     $('.contents').addClass('center');
 
 
-  }, 300 * (typingLength + 1));
+  }, 300 * (introLen + 1));
+
+
+
+
+
+
 
 
 
@@ -132,6 +221,7 @@ $(document).ready(function () {
 
       $(this).find('.webPic').addClass('on');
 
+
       clearInterval(webPicOn);
 
     }
@@ -141,7 +231,19 @@ $(document).ready(function () {
 
 
 
-// 웹사이트 사진 리스트들에 각각의 사진 지정
+
+
+
+
+
+
+
+
+
+
+
+
+  // 웹사이트 사진 리스트들에 각각의 사진 지정
 
 
   const webNum = $('.webPic li').length;
@@ -187,24 +289,24 @@ $(document).ready(function () {
 
       // 리스트들은 0.7초마다 0.5초동안 투명도 변경
 
-      $('.webPic li').animate({'opacity':`${0.3 * webCount}`},500);
+      $('.webPic li').animate({ 'opacity': `${0.3 * webCount}` }, 500);
 
 
       // 처음리스트가 내려올경우
 
-      if(webCount === 1){
+      if (webCount === 1) {
 
         // 처음 리스트만 내려오게 지정
 
-        $('.webPic li').eq(webCount -1).css({ 'bottom': '100%' }).animate({ 'bottom': '0%' });
+        $('.webPic li').eq(webCount - 1).css({ 'bottom': '100%' }).animate({ 'bottom': '0%' });
       }
 
-      else{
+      else {
 
         // 그외에는 다음리스트를 내리면서 리스트가 내려오면서 보임
 
         $('.webPic li').eq(webCount - 2).css({ 'bottom': 0 }).animate({ 'bottom': '-100%' });
-        $('.webPic li').eq(webCount -1 ).css({ 'bottom': '100%' }).animate({ 'bottom': '0%' });
+        $('.webPic li').eq(webCount - 1).css({ 'bottom': '100%' }).animate({ 'bottom': '0%' });
       }
 
 
@@ -214,8 +316,9 @@ $(document).ready(function () {
 
       setTimeout(() => {
 
-        
+
         $('.contents').addClass('show');
+
 
 
         clearInterval(webChange);
@@ -236,12 +339,134 @@ $(document).ready(function () {
 
 
 
+
+  const titleArr = ['jeong seung hoon', 'portfolio'];
+
+
+
+
+
+
+  let lineChange;
+
+
+  let titleTyping = setInterval(() => {
+
+
+
+
+    if ($('.contents').hasClass('show')) {
+
+
+
+
+      txtTypeEffect(titleArr[0], $('.contents>.main .project>h3 span:nth-child(1)'), 50)
+
+
+      setTimeout(() => {
+
+        lineChange = true;
+
+      }, titleArr[0].length * 50)
+
+
+
+      clearInterval(titleTyping)
+
+
+
+
+
+
+    }
+
+
+
+  }, 1000);
+
+
+
+
+
+
+
+
+
+
+  let titleChangeTyping = setInterval(() => {
+
+
+
+    if (lineChange) {
+
+      txtTypeEffect(titleArr[1], $('.contents>.main .project>h3 span:nth-child(2)'), 50)
+
+
+      clearInterval(titleChangeTyping)
+
+    }
+
+
+
+
+
+  }, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // nav 리스트 클릭시에 
 
 
 
 
-  $('.contents>nav ul li').click(function(){
+  $('.contents>nav ul li').click(function () {
 
     let I = $(this).index();
 
@@ -267,23 +492,23 @@ $(document).ready(function () {
 
 
     // 0.4초 후에 다시 되돌아오면서 타이틀 상수에 있는 텍스트를 제목으로 받아옴
-    setTimeout(()=>{
+    setTimeout(() => {
       $('.contents>h2').removeClass('fold');
       $('.contents>h2').text(title);
 
-    },400);
+    }, 400);
 
 
 
   })
 
 
-  
+
 
   // 프로필 영역의 텍스트 클릭시,
   // nav의 프로필 리스트 클릭처리
 
-  $('.contents>.main .profile h3').click(function(){
+  $('.contents>.main .profile h3').click(function () {
 
     $('.contents>nav ul li').eq(1).trigger('click');
 
@@ -296,11 +521,26 @@ $(document).ready(function () {
   // nav의 프로젝트 리스트 클릭처리
 
 
-  $('.contents>.main .project h4').click(function(){
+
+
+
+  $('.contents>.main .project h4').click(function () {
 
     $('.contents>nav ul li').eq(2).trigger('click');
 
+
   })
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -308,12 +548,13 @@ $(document).ready(function () {
   // main 버튼 클릭시
   // 모든 컨텐츠 영역 제자리
 
-  $('.contents>nav ul li').eq(0).click(function(){
+  $('.contents>nav ul li').eq(0).click(function () {
 
     $('.contents .main>div').removeClass('on');
     $('.contents .main>div').removeClass('off');
 
 
+    $('.contents>.main .project .webList li').removeClass('on');
     $('.contents>.main .profile>.mainProfile').fadeOut();
   })
 
@@ -322,14 +563,14 @@ $(document).ready(function () {
   // 컨텐츠 영역 초기화 후에 프로필 영역 등장,
   // 프로젝트 영역 퇴장
 
-  $('.contents>nav ul li').eq(1).click(function(){
+  $('.contents>nav ul li').eq(1).click(function () {
 
     $('.contents .main>div').removeClass('on');
     $('.contents .main>div').removeClass('off');
     $('.contents .main>.profile').addClass('on');
     $('.contents .main>.project').addClass('off');
 
-
+    $('.contents>.main .project .webList li').removeClass('on');
 
     $('.contents>.main .profile>.mainProfile').fadeIn();
 
@@ -341,14 +582,33 @@ $(document).ready(function () {
   // 컨텐츠 영역 초기화 후에 프로젝트 영역 등장,
   // 프로필 영역 퇴장
 
-  $('.contents>nav ul li').eq(2).click(function(){
+  $('.contents>nav ul li').eq(2).click(function () {
     $('.contents .main>div').removeClass('on');
     $('.contents .main>div').removeClass('off');
     $('.contents .main>.project').addClass('on');
     $('.contents .main>.profile').addClass('off');
 
 
-    
+
+    const projectLen = $('.contents>.main .project .webList li').length;
+
+
+    for (let p = 0; p < projectLen; p++) {
+
+
+      setTimeout(() => {
+
+        $('.contents>.main .project .webList li').eq(p).addClass('on');
+
+
+      }, (p) * 500);
+
+    }
+
+
+
+
+
     $('.contents>.main .profile>.mainProfile').fadeOut();
   })
 
@@ -367,21 +627,21 @@ $(document).ready(function () {
 
   // 페이지 버튼 클릭시 프로필 다음 화면 이동
 
-  $('.contents>.main .profile>.mainProfile .page').click(function(){
+  $('.contents>.main .profile>.mainProfile .page').click(function () {
 
 
     $('.contents>.main .profile>.mainProfile>div').toggleClass('on');
 
-    if($('.contents>.main .profile>.mainProfile>.status').hasClass('on')){
+    if ($('.contents>.main .profile>.mainProfile>.status').hasClass('on')) {
 
       // for(let s = 0; s < status.length; s++){
 
       //   setTimeout(() => {
       //     $('.contents>.main .profile>.mainProfile>div>ul .skill li').eq(s).find('.mybar').stop().animate({'width':`${status[s]}%`},800);
-          
-       
+
+
       //   }, 300 * s);
-        
+
 
 
 
@@ -392,12 +652,12 @@ $(document).ready(function () {
 
 
 
-      status.forEach((s,i) => {
+      status.forEach((s, i) => {
 
         setTimeout(() => {
-          $('.contents>.main .profile>.mainProfile>div>ul .skill li').eq(i).find('.mybar').stop().animate({'width':`${s}%`},800);
+          $('.contents>.main .profile>.mainProfile>div>ul .skill li').eq(i).find('.mybar').stop().animate({ 'width': `${s}%` }, 800);
 
-          $('.contents>.main .profile>.mainProfile>div>ul .skill li').find('span').show();
+          $('.contents>.main .profile>.mainProfile>div>ul .skill li').find('h5').show();
 
           let count = 0;
 
@@ -406,14 +666,14 @@ $(document).ready(function () {
 
 
             count++;
-            
-            if(count === s){
+
+            if (count === s) {
 
               clearInterval(persentCount)
 
             }
 
-            $('.contents>.main .profile>.mainProfile>div>ul .skill li').eq(i).find('span').text(`${count}%`);
+            $('.contents>.main .profile>.mainProfile>div>ul .skill li').eq(i).find('h5').text(`${count}%`);
 
 
           }, 800 / s);
@@ -421,7 +681,7 @@ $(document).ready(function () {
 
 
 
-       
+
         }, 300 * i);
 
       });
@@ -441,15 +701,15 @@ $(document).ready(function () {
 
 
 
-    else{
+    else {
 
-      for(let s = 0; s < status.length; s++){
-
-        
-        $('.contents>.main .profile>.mainProfile>div>ul .skill li').eq(s).find('.mybar').stop().animate({'width':`0%`},s * 100);
+      for (let s = 0; s < status.length; s++) {
 
 
-        $('.contents>.main .profile>.mainProfile>div>ul .skill li').find('span').hide();
+        $('.contents>.main .profile>.mainProfile>div>ul .skill li').eq(s).find('.mybar').stop().animate({ 'width': `0%` }, s * 100);
+
+
+        $('.contents>.main .profile>.mainProfile>div>ul .skill li').find('.page').hide();
       }
 
 
@@ -471,7 +731,7 @@ $(document).ready(function () {
   // 컨셉뷰 클릭시 컨셉뷰 화면 등장
 
 
-  $('.contents>.main .project .webList li .txt .conceptBtn').click(function(){
+  $('.contents>.main .project .webList li .txt .conceptBtn').click(function () {
 
 
     $('.concept_view').addClass('on');
@@ -480,12 +740,13 @@ $(document).ready(function () {
     $('.concept_view').scrollTop(0);
 
 
-    let conI = $(this).parents('li').index() + 1;
+    let conI = $(this).parents('li').index();
 
 
+    $('.concept_view img').hide();
 
-    $('.concept_view img').attr('src',`img/view${conI}.png`);
 
+    $('.concept_view img').eq(conI).show();
 
   })
 
@@ -494,7 +755,7 @@ $(document).ready(function () {
 
 
 
-  $('.concept_view').click(function(){
+  $('.concept_view').click(function () {
 
     $(this).removeClass('on')
 
